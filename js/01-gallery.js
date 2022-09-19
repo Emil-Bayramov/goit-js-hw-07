72de168f939fbad1296cfc930cd5aa6982e4c0bf
Emil-Bayramov <105307406+Emil-Bayramov@users.noreply.github.com>
2022-09-19 12:27:45 +0300

    20220919/1227
    hope final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const markUp = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`
+  )
+  .join("");
+
+  gallery.insertAdjacentHTML("afterbegin", markUp);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+});
